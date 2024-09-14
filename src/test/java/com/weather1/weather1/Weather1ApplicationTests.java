99861335be8c3c5ab3f469fa643246930174dd41
DobermanDD00 <doberman.dd00@gmail.com>
2024-09-15 00:30:48 +0300

    equals weather, but it's work!

---
+++ src/test/java/com/weather1/weather1/Weather1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weather1.weather1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Weather1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
